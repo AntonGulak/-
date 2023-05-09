dd590d0037c15c5840cb83b43d04352dbb750d05
AntonGulak <55970327+AntonGulak@users.noreply.github.com>
2023-05-10 00:59:17 +0700

    add neyro

---
+++ neyro/graph.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+from PIL import Image, ImageDraw
+from tensorflow.keras.models import load_model
+import numpy as np
+
+# Загрузка обученной модели
+model = load_model('my_model.h5')
+
+# Функция predict_digit принимает изображение img и изменяет его размер до 28x28 пикселей для соответствия с моделью
+# Преобразуем изображение в массив NumPy, изменяем его форму и нормализуем значения пикселей, деля их на 255.
+# Также для точности переводим изображение в градиент серого
+
+def predict_digit(img):
+    img = img.resize((28, 28))
+    img = img.convert('L')
+    img = np.array(img)
+    img = img.reshape(1, 28, 28)
+    img = img / 255.0
+
+    # Получаем предсказание модели для изображения, выводим результат на экран и возвращаем индекс
+    # максимального значения в массиве предсказаний (это и есть распознанная цифра).
+    prediction = model.predict(img)
+    return np.argmax(prediction)
+
+# Функция on_paint обрабатывает событие рисования на холсте (canvas) приложения.
+# Она создает круги на холсте и линии на изображении image1 в соответствии с перемещением курсора мыши.
+def on_paint(event):
+    x1, y1 = (event.x - 5), (event.y - 5)
+    x2, y2 = (event.x + 5), (event.y + 5)
+    canvas.create_oval(x1, y1, x2, y2, fill='black', width=10)
+    draw.line([x1, y1, x2, y2], fill='black', width=10)
+
+# Функция clear_canvas очищает холст и изображение image1, заполняя их белым цветом.
+def clear_canvas():
+    canvas.delete('all')
+    draw.rectangle([(0, 0), (280, 280)], fill='white')
+
+# Функция recognize_digit вызывает функцию predict_digit для копии изображения image1 и выводит распознанную цифру в виджете result_label.
+def recognize_digit():
+    img = image1.copy()
+    digit = predict_digit(img)
+    result_var.set(f"Распознанная цифра: {digit}")
+
+window = tk.Tk()
+window.title("Распознавание цифр")
+
+# Создаём форму приложения
+
+canvas = tk.Canvas(window, width=280, height=280, bg='white')
+canvas.grid(row=0, column=0, pady=2, sticky='W', columnspan=2)
+
+image1 = Image.new('RGB', (280, 280), 'white')
+
+draw = ImageDraw.Draw(image1)
+
+canvas.bind('<B1-Motion>', on_paint)
+
+result_var = tk.StringVar()
+result_label = tk.Label(window, textvariable=result_var, font=("Arial", 14))
+result_label.grid(row=0, column=2, padx=2)
+
+clear_button = tk.Button(window, text="Очистить", command=clear_canvas)
+clear_button.grid(row=1, column=0, pady=2)
+
+recognize_button = tk.Button(window, text="Распознать", command=recognize_digit)
+recognize_button.grid(row=1, column=1, pady=2)
+
+exit_button = tk.Button(window, text="Выход", command=window.quit)
+exit_button.grid(row=1, column=2, pady=2)
+
+window.mainloop()
---
+++ neyro/model.py
@@ -0,0 +1,41 @@
+import numpy as np
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.datasets import mnist
+
+# Загружаем данные рукописных цифр MNIST и разделяем их на обучающую и тестовую выборку.
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Преобразуем метки обучающей и тестовой выборки (y_train и y_test) в формат one-hot encoding.
+# Это представление меток позволяет нейронной сети лучше обучаться на задачах многоклассовой классификации.
+temp = []
+for i in range(len(y_train)):
+    temp.append(to_categorical(y_train[i], num_classes=10))
+y_train = np.array(temp)
+
+temp = []
+for i in range(len(y_test)):
+    temp.append(to_categorical(y_test[i], num_classes=10))
+y_test = np.array(temp)
+
+
+# Создаем последовательную модель нейронной сети (Sequential). Входной слой Flatten преобразует двумерные изображения 28x28 пикселей
+# в одномерный массив. Следующий слой – это полносвязный слой с 5 нейронами и функцией активации sigmoid.
+# Выходной слой – это полносвязный слой с 10 нейронами (по количеству классов) и функцией активации softmax.
+model = Sequential()
+model.add(Flatten(input_shape=(28,28)))
+model.add(Dense(5, activation='sigmoid'))
+model.add(Dense(10, activation='softmax'))
+
+# Компилируем модель, указывая функцию потерь (categorical_crossentropy), оптимизатор (adam) и метрику для оценки качества обучения (точность, acc).
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['acc'])
+
+#Обучаем модель на обучающих данных, указывая количество эпох (5) и данные для валидации (тестовая выборка).
+model.fit(X_train, y_train, epochs=5,
+          validation_data=(X_test,y_test))
+
+#Сохраняем обученную модель в файл "my_model.h5" для дальнейшего использования.
+model.save("my_model.h5")
